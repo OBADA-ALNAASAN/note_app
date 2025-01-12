89231566edcdc464167bff187a8db7bdf12eef93
obada <obadaalnaasan@gmail.com>
2025-01-12 16:15:24 +0300

    custom AppBar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/note_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Scaffold());
+    return const SafeArea(child: Scaffold(
+      body:NoteViewBody(
+        
+      )
+       ,
+    ));
   }
 }
