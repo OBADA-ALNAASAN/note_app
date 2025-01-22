8b96866433e7835ea20f17b294b7fb6401cd704b
obada <obadaalnaasan@gmail.com>
2025-01-22 13:59:37 +0300

    add note state

--- lib/cubits/note_cubit/note_cubit_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'note_cubit_state.dart';
-
-class NoteCubitCubit extends Cubit<NoteCubitState> {
-  NoteCubitCubit() : super(NotesCubitsInitial());
-}
--- lib/cubits/note_cubit/note_cubit_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'note_cubit_cubit.dart';
-
-@immutable
-sealed class NoteCubitState {}
-
-final class NotesCubitsInitial extends NoteCubitState {}
